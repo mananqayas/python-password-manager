8f0e7a80acd110bd2ee71cdcc8c5fb2f9acbd591
digitalifylabs-9568 <digitalifylabs@gmail.com>
2025-09-08 20:49:17 -0400

    added comments

--- classes.py
+++ classes.py
@@ -6,7 +6,9 @@ from typing import List
 
 
 # Password Class
+# Password class is used to create password objects with attributes (title, url, username, password)
 class Password:
+    # Init method is initialzied when the object is created
     def __init__(self, title, url, username, password):
         self.title = title
         self.url = url
@@ -14,25 +16,26 @@ class Password:
         self.password = password
 
 # User Class
+# User Class creates user object, saves userdata in userdata.csv, and creates Password objects to save user passwords data in passwords.csv
 class User:
+    # Init method takes defauls file name for storing passwords
     def __init__(self, email, password, file_name="passwords.csv"):
         self.email = email
         self.password = password
         self.file_name = file_name
         self.passwords: List[Password] = []
+
+    # Create password method is used to store user passwords in the passwords.csv file, it returns successful message when password is saved, and also shows error message if the same title is used in one of the previously saved passwords by the user
     def create_password(self, title, url, username, password):
         user_passwords = []
         with open(self.file_name, 'r', newline='') as file:
             reader = csv.reader(file)
             for row in reader:
                 if row and row[0] == self.email:
-                    user_passwords.append(row[1])
-      
-  
+                    user_passwords.append(row[1]) 
            
         if len(user_passwords) > 0:
          
-          
             for pass_title in user_passwords:
            
            
@@ -65,7 +68,7 @@ class User:
     
 
            
-
+    # This method is used to get all user specific passwords from the passwords.csv file
     def get_all_passwords(self):
         user_passwords = []
         with open(self.file_name, 'r',newline='') as file:
@@ -87,6 +90,8 @@ class User:
                 print(f"{index+1}: title: {value[1]}, url: {value[2]}, username: {value[3]}, password: {value[4]}")
                 print('--------------------------------')
            
+
+    # This method is used to delete one password from the list of logged in user passwords by matching title. Proper errors are setup in this method to guide the user.
     def delete_password(self, title):
         user_paswords = []
         removed = None
@@ -121,10 +126,12 @@ class User:
 
 
 # Password Manager Class
+# PasswordManager Class is used to register and login the users, is also manages user authentication state using logged_in_user attribute
 class PasswordManager:
     def __init__(self, file_name="userdata.csv"):
         self.logged_in_user: User | None = None
         self.file_name = file_name
+    # register method is used to register user using its username and password
     def register(self, email, password):
         regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+$'
         if re.match(regex, email):
@@ -144,6 +151,8 @@ class PasswordManager:
                         print("Signed up successfully")
         else:
             print("Invalid email address provided")
+   
+    # login method logins the user with proper errors, and sets the log in state on success
     def login(self, email, password):
         regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+$'
         hash = hashlib.sha256(str(password).encode()).hexdigest()
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from classes import User, PasswordManager, Password
 import re
-
+# This function is called when user selects login while interacting with our program
 def login():
             pm = PasswordManager()
             correct_email = False
             while correct_email == False:
                 email = input("Enter your email address\n")
                 regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+$'
+                # verifiying email
                 if re.match(regex, email):
                      password = input("Enter your password\n")
                      pm.login(email, password)
+                     #pm.login returns user state and we are checking if user object was created after logging in, if its None, else block is executed
                      if pm.logged_in_user:
                          print(f"user logged in as {pm.logged_in_user.email}")
                          logged_in = True
@@ -20,11 +22,13 @@ def login():
                              print("Enter \"delete-password\" to delete a password\n")
                              print("Enter \"logout\" to logout\n")
                              ask = input("Choose your option\n")
+                             # When user enters get-password
                              if ask == "get-passwords":
                                  print("=========ALL PASSWORDS=========")
                                  pm.logged_in_user.get_all_passwords()
                                  print("=========ALL PASSWORDS=========")
-                                 
+
+                            # When user enters create-password
                              elif ask == "create-password":
                                  print("----------------------------------------------")
                                  title = input("Enter the password item title\n")
@@ -36,13 +40,16 @@ def login():
                                  password = input("What is password for this account?\n")
                                  print("----------------------------------------------")
                                  pm.logged_in_user.create_password(title, url, username, password)
-
+                            
+                            # When user enters delete-password
                              elif ask == "delete-password":
                                  print("=========ALL PASSWORDS ARE HERE=========")
                                  pm.logged_in_user.get_all_passwords()
                                  print("Please select the title from the above passwords")
                                  title = input("Enter the password item title you want to delete\n")
                                  pm.logged_in_user.delete_password(title)
+
+                             # When user enters logout
                              elif ask == "logout":
                                  print("----------------------------------------------")
                                  print("You are logged out successfully! Goodbye")
@@ -56,7 +63,7 @@ def login():
                 else:
                     print("Enter a valid email address")
 
-
+# Main function starts running when our program starts, it uses while loops to keep asking the user correct inputs until he gets it right.
 def main():
     onboarding = False
     while onboarding == False:
